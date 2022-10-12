5a3f69e9a8c964c30f60b1ab5715e1d2f7b05dc1
manuela <manuelavalenciafranco08@gmail.com>
2022-10-12 10:48:30 -0500

    oli

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-print("Hola Mundo")
+class nodo:
+    def crear_nodo(self, data = None, next = None):
+        self.data = data
+        self.next = next
 
-print("I love matemáticas")
+class lista: 
+    def crear_lista(self):
+        self.head = None
+
+def lista_vacia(self):
+    return self.head == None
+
+def insertar_elemento(self, dato):
+    if self.head == None:
+        self.head = node(data = dato)
+        return
+    actual = self.head
+    while actual.next:
+        actual = actual.next
+    actual.next = node(data=dato)
+
+def eliminar_nodo(self, elemento):
+    
+
+
+
