6bb0bd469f354d1d006d0d7b881a3adc3159c6fb
Diego G <diegogalvis0905@hotmail.com>
2022-10-24 22:04:10 -0500

    Handling errors

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import os
-import msvcrt
-import sys
+import os, msvcrt, sys, time
 
 class nodo:
     def __init__(self, data = None, next = None):
         self.data = data
         self.next = next
-
 
 class lista: 
     def __init__(self):
@@ -82,12 +79,25 @@ def wait(): #Wait until user press a key
 
 class main():
     s = lista()
+    choice = 0
 
     while True:
         MostrarMenu()
-        choice = int(input("Ingrese una opción "))
+        
+        try:
+            choice = int(input("Ingrese una opción "))
+        except ValueError:
+            print("Debes ingresar uno de los valores en el menú")
+            print("Intentalo de nuevo")
+            wait()
+            cls()
 
-        if choice == 1:
+        if choice < 1 or choice > 5:
+            print("Fuera de rango, intentalo de nuevo")
+            time.sleep(3)
+            cls()
+            continue
+        elif choice == 1:
             dato = input("Ingrese el número ")
             s.insertar_elemento(dato)
             cls()
@@ -114,7 +124,6 @@ class main():
             cls()
         
         elif choice == 4:
-
             print("Con este método actualizaras el archivo de texto ")
             print("y se te mostrarán los elementos que estén actualmente ")
             print("en la lista")
